3c5b6499e0eb2e7fd5e103f28c880906e1eb6715
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-01-06 10:06:07 +0200

    feat: react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home } from './pages/Home.tsx';
+import { Contact } from './pages/Contact.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/template-vite-react-ts/',
+    element: <App />,
+    children: [
+      {
+        path: 'template-vite-react-ts/',
+        element: <Home />,
+      },
+      {
+        path: 'template-vite-react-ts/contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
